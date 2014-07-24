2c2177b7d0d3a34b410315a465300007b701c1be
smith.h.neil <smith.h.neil@gmail.com>
2014-07-24 01:42:10 -0500

    feat: add identity models in infrastructure

---
+++ src/tc-dev.Core.Infrastructure/Identity/Models/AppIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace tc_dev.Core.Infrastructure.Identity.Models
+{
+    public class AppIdentityUser 
+        : IdentityUser<int, AppIdentityUserLogin, AppIdentityUserRole, AppIdentityUserClaim>
+    {
+    }
+}
---
+++ src/tc-dev.Core.Infrastructure/Identity/Models/AppIdentityUserRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace tc_dev.Core.Infrastructure.Identity.Models
+{
+    public class AppIdentityUserRole : IdentityUserRole<int>
+    {
+    }
+}
