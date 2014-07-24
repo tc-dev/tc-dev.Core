6d873e241860351f2ee428dd8cfdaa3a98a1ae3b
smith.h.neil <smith.h.neil@gmail.com>
2014-07-24 01:28:46 -0500

    feat: add NLogLogger

---
+++ src/tc-dev.Core.Infrastructure/Logging/NLogLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NLog;
+using tc_dev.Core.Service;
+
+namespace tc_dev.Core.Infrastructure.Logging
+{
+    public class NLogLogger : ILogger
+    {
+        private static readonly Lazy<NLogLogger> LazyLogger = 
+            new Lazy<NLogLogger>(() => new NLogLogger());
+        private static readonly Lazy<NLog.Logger> LazyNLogger = 
+            new Lazy<Logger>(LogManager.GetCurrentClassLogger);
+
+        private NLogLogger() { }
+
+        public static ILogger Instance {
+            get { return LazyLogger.Value; }
+        }
+
+        public void Log(string message) {
+            LazyNLogger.Value.Info(message);
+        }
+
+        public void Log(Exception exception) {
+            LazyNLogger.Value.Error(exception);
+        }
+    }
+}
