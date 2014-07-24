2d5ede7d53d1e0822f001b68af16a19d36468a99
smith.h.neil <smith.h.neil@gmail.com>
2014-07-24 12:14:09 -0500

    feat(core): add AuthenticationManager interface

---
+++ src/tc-dev.Core/Identity/IAppAuthenticationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using tc_dev.Core.Identity.Models;
+
+namespace tc_dev.Core.Identity
+{
+    public interface IAppAuthenticationManager
+    {
+        IEnumerable<AppAuthenticationDescription> GetExternalAuthenticationTypes();
+
+        AppExternalLoginInfo GetExternalLoginInfo();
+        Task<AppExternalLoginInfo> GetExternalLoginInfoAsync();
+
+        void SignIn(params ClaimsIdentity[] identities);
+
+        void SignOut(params string[] authenticationTypes);
+    }
+}
