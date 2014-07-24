9a081e2eab7592c525b200a5464894e6df4ddedc
smith.h.neil <smith.h.neil@gmail.com>
2014-07-24 11:19:02 -0500

    fix(IAppRoleManager): add (missing) synchronous methods

--- src/tc-dev.Core/Identity/IAppRoleManager.cs
+++ src/tc-dev.Core/Identity/IAppRoleManager.cs
@@ -9,13 +9,23 @@ namespace tc_dev.Core.Identity
     {
         AppIdentityResult Create(AppRole role);
         Task<AppIdentityResult> CreateAsync(AppRole role);
+
+        AppIdentityResult Delete(int roleId);
         Task<AppIdentityResult> DeleteAsync(int roleId);
+
+        AppRole FindById(int roleId);
         Task<AppRole> FindByIdAsync(int roleId);
+
         AppRole FindByName(string roleName);
         Task<AppRole> FindByNameAsync(string roleName);
+
         IEnumerable<AppRole> GetRoles();
         Task<IEnumerable<AppRole>> GetRolesAsync();
+
+        bool RoleExists(string roleName);
         Task<bool> RoleExistsAsync(string roleName);
+
+        AppIdentityResult Update(int roleId, string roleName);
         Task<AppIdentityResult> UpdateAsync(int roleId, string roleName);
     }
 }
