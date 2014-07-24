f55f0cefcb4bbeb4ecbb6b66808af0ce4d0a105f
smith.h.neil <smith.h.neil@gmail.com>
2014-07-23 22:14:41 -0500

    feat: add throw if null exception utilities

---
+++ src/tc-dev.Core.Common/Utilities/ExceptionUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tc_dev.Core.Common.Utilities
+{
+    public static class ExceptionUtils
+    {
+        public static void ThrowIfNull<T>(
+            this T source, string paramName = null, string message = null) 
+                where T : class
+        {
+            if (source == null)
+                throw new ArgumentNullException(paramName, message);
+        }
+
+        public static void ThrowIfNull<T>(
+            this T? source, string paramName = null, string message = null)
+                where T : struct 
+        {
+            if (source == null)
+                throw new ArgumentNullException(paramName, message);
+        }
+    }
+}
---
+++ tests/tc-dev.Core.Common.UnitTests/Utilities/ExceptionUtilsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using tc_dev.Core.Common.Utilities;
+
+namespace tc_dev.Core.Common.UnitTests.Utilities
+{
+	[TestFixture]
+    public class ExceptionUtilsTests
+    {
+	    [TestCase(null, null)]
+	    [TestCase("paramName", "message")]
+	    public void ThrowIfNull_NullClass_ThrowsException(string paramName, string message) {
+            string nullThing = null;
+
+	        Assert.Throws<ArgumentNullException>(() => nullThing.ThrowIfNull(paramName, message));
+	    }
+
+	    [TestCase(null, null)]
+	    [TestCase("paramName", "message")]
+	    public void ThrowIfNull_NotNullClass_DoesNotThrowException(string paramName, string message) {
+	        string notNullThing = "imNotNull";
+
+	        Assert.DoesNotThrow(() => notNullThing.ThrowIfNull(paramName, message));
+	    }
+
+        [TestCase(null, null)]
+        [TestCase("paramName", "message")]
+        public void ThrowIfNull_NullStruct_ThrowsException(string paramName, string message) {
+            Nullable<int> nullThing = null;
+
+            Assert.Throws<ArgumentNullException>(() => nullThing.ThrowIfNull(paramName, message));
+        }
+
+        [TestCase(null, null)]
+        [TestCase("paramName", "message")]
+        public void ThrowIfNull_NotNullStruct_DoesNotThrowException(string paramName, string message) {
+            Nullable<int> notNullThing = 42;
+
+            Assert.DoesNotThrow(() => notNullThing.ThrowIfNull(paramName, message));
+        }
+    }
+}
