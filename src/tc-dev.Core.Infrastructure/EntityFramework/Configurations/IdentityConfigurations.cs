cfc6ff14cc96736a2b3bf44fd4dd97dbc9f8c7c6
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 09:41:41 -0500

    feat: add identity entity configuration extensions

---
+++ src/tc-dev.Core.Infrastructure/EntityFramework/Configurations/IdentityConfigurations.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using tc_dev.Core.Infrastructure.Identity.Models;
+
+namespace tc_dev.Core.Infrastructure.EntityFramework.Configurations
+{
+    public static class IdentityConfigurations
+    {
+        public static void Configure(this EntityTypeConfiguration<AppIdentityUser> source) {
+            source
+                .ToTable("IdentityUser")
+                .Property(m => m.Id)
+                    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+        public static void Configure(this EntityTypeConfiguration<AppIdentityRole> source) {
+            source
+                .ToTable("IdentityRole")
+                .Property(m => m.Id)
+                    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+
+        public static void Configure(this EntityTypeConfiguration<AppIdentityUserRole> source) {
+            source
+                .ToTable("IdentityUserRole");
+        }
+
+        public static void Configure(this EntityTypeConfiguration<AppIdentityUserLogin> source) {
+            source
+                .ToTable("IdentityUserLogin");
+        }
+
+        public static void Configure(this EntityTypeConfiguration<AppIdentityUserClaim> source) {
+            source
+                .ToTable("IdentityUserClaim")
+                .Property(m => m.Id)
+                    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
