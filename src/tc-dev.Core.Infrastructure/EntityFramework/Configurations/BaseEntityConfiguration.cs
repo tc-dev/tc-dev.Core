a79fd216d6a8678908d386025423bbf8044f2aa8
smith.h.neil <smith.h.neil@gmail.com>
2014-07-24 19:26:45 -0500

    feat(data): add BaseEntityConfiguration entity type configuration

---
+++ src/tc-dev.Core.Infrastructure/EntityFramework/Configurations/BaseEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using tc_dev.Core.Domain;
+
+namespace tc_dev.Core.Infrastructure.EntityFramework.Configurations
+{
+    public abstract class BaseEntityConfiguration<TEntity> : EntityTypeConfiguration<TEntity>
+        where TEntity : BaseEntity
+    {
+        protected BaseEntityConfiguration() {
+
+            HasKey(m => m.Id);
+
+            Property(m => m.DateCreated)
+                .IsRequired();
+
+            Property(m => m.DateLastModified)
+                .IsOptional();
+
+        }
+    }
+}
